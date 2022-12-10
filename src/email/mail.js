55282eeaac33914df02a8af784edecaba89e3a50
mab-rook <mubarakmahmunalaka@gmail.com>
2022-12-10 07:38:29 +0000

    updated files

---
+++ src/email/mail.js
@@ -0,0 +1,51 @@
+const mailer = require("nodemailer");
+const [Hello] = require("./hello_template")
+
+const getEmailData = (to, name, template) => {
+  let data = null;
+  switch (template) {
+    case "hello":
+      data = {
+        from: "Mabrook <mubarakmahmunalaka@gmail.com>",
+        to,
+        subject: `hello `,
+        html: Hello()
+      }
+      break
+    
+    case "thanks":
+      data = {
+        from: "Mabrook <mubarakmahmunalaka@gmail.com>",
+        to,
+        subject: `hello`,
+        html: Thanks()
+      }
+      break;
+    default:
+      data;
+  }
+  return data
+}
+
+const sendEmail = (to, type) => {
+  const smtpTransport = mailer.createTransport({
+    services: "Gmail",
+    auth: {
+      user: "mubarakmahmunalaka@gmail.com",
+      pass: "wrzjbavyxhvthmks"
+    }
+
+    
+  })
+  const mail = getEmailData(to, type)
+  smtpTransport.sendMail(mail, function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(" email sent successfully")
+    }
+    smtpTransport.close();
+  })
+}
+
+module.exports ={ sendEmail}
---
+++ thank_template.js
@@ -0,0 +1,22 @@
+const Thanks = data => {
+  return `
+  <!DOCTYPE html>
+  <html style="margin:0; padding: 0;">
+    <head>
+      <title>Hello</title>
+    </head>
+    <body>
+      <br/>
+      <br/>
+      <div>Thanks</div>
+      <br/>
+      <br/>
+    
+    </body>
+  
+  </html>
+  
+  `
+}
+
+module.exports = {Thanks}
