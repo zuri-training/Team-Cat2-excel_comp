e03705b4f82cd1f0b826519df2359476c817799b
AllenKayy <allisonoyinkansola1@gmail.com>
2022-12-17 09:18:49 +0100

    renamed client

--- src/server.js
+++ src/server.js
@@ -27,10 +27,10 @@ app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   //*Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('../client/build'));
   
   app.get('*', (req, res) => 
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   );
 }
 
