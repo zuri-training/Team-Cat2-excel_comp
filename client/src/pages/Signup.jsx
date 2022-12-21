aa32757b2cffc6d7541f4f4ea903dd4561e69fed
AllenKayy <allisonoyinkansola1@gmail.com>
2022-12-21 11:51:08 +0100

    ...

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -5,7 +5,7 @@ import {FcGoogle} from 'react-icons/fc'
 import {Link} from 'react-router-dom'
 import {AiFillEyeInvisible,AiFillEye} from 'react-icons/ai'
 import { useState } from 'react'
-import axios from "axios"
+// import axios from "axios"
 
 
 const Signup = () => {
